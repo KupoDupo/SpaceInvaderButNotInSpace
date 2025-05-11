3afbf7dbe0b4df03ec62f899a7d32e202d32fb81
KupoDupo <lnkleijnen@gmail.com>
2025-05-11 14:45:52 -0700

    Done with bare minimum -> to do extra credit features

--- src/Scenes/PlanesFight.js
+++ src/Scenes/PlanesFight.js
@@ -1,10 +1,10 @@
 // TODO Essential:
-// Fix GameOver() to reset scene and empty all arrays (clear out all the sprites essentially)
-// Slow down medium ship bullet fire rate 
-// Make medium ship hitable - take 3 hits - collidable now but doesn't take 3 hitsd (only 1)
+// DONE: Fix GameOver() to reset scene and empty all arrays (clear out all the sprites essentially)
+// DONE: Slow down medium ship bullet fire rate 
+// DONE: Make medium ship hitable - take 3 hits - collidable now but doesn't take 3 hitsd (only 1)
 // slow down player fire rate
-// pyramid spawn 3 times
-// two medium enemies, one on left one on right
+// DONE: pyramid spawn 3 times
+// DONE: two medium enemies, one on left one on right
 
 // TODO Optional:
 // put in background?
@@ -17,11 +17,9 @@ class PlanesFight extends Phaser.Scene {
         super("planesFight");
 
         this.my = {sprite: {}, text: {}};
-
-        this.my.sprite.bullet = [];   
+   
         this.maxBullets = 20;           // Don't create more than this many bullets
         
-        this.my.sprite.enemy_bullet = []; 
         this.maxEnemyBullets = 20;
 
         this.myScore = 0;       // record a score as a class variable
@@ -62,8 +60,6 @@ class PlanesFight extends Phaser.Scene {
         let startY = 50; // Starting Y position for the pyramid
         let spacing = 40; // Spacing between ships
     
-        my.sprite.enemies = []; // Array to store enemy ships
-    
         // Create the pyramid (3 rows: 1, 2, 3 ships)
         for (let row = 0; row < 3; row++) {
             for (let col = 0; col <= row; col++) {
@@ -79,7 +75,7 @@ class PlanesFight extends Phaser.Scene {
     }
 
     // Helper Function to Check if All Enemies are Defeated (for respawn and game over)
-    areAllEnemiesDefeated() {
+    areAllSmallEnemiesDefeated() {
         return this.my.sprite.enemies.every(enemy => !enemy.visible);
     }
 
@@ -94,6 +90,12 @@ class PlanesFight extends Phaser.Scene {
     create() {
         let my = this.my;
 
+        //Initializing Arrays
+        this.my.sprite.enemy_bullet = []; 
+        this.my.sprite.bullet = []; 
+        my.sprite.enemies = [];
+
+
         my.sprite.player = this.add.sprite(game.config.width/2, game.config.height - 40, "player");
         my.sprite.player.setScale(2.0);
 
@@ -106,12 +108,23 @@ class PlanesFight extends Phaser.Scene {
         my.sprite.medium_enemy.direction = 1;
         my.sprite.medium_enemy.speed = 1;
         my.sprite.medium_enemy.lastFired = 0;
+        my.sprite.medium_enemy.health = 3;
+
+        my.sprite.medium_enemy_left = this.add.sprite(100, 80, "m_enemy");
+        my.sprite.medium_enemy_left.scorePoints = 50;
+        my.sprite.medium_enemy_left.setScale(2.0);
+        my.sprite.medium_enemy_left.setFlipY(true);
+        my.sprite.medium_enemy_left.direction = 1;
+        my.sprite.medium_enemy_left.speed = 1;
+        my.sprite.medium_enemy_left.lastFired = 0;
+        my.sprite.medium_enemy_left.health = 3;
 
         // I know I probably could've done the set path stuff we went over but this seemed more precise
         this.time.addEvent({
             delay: 2000, // 2 seconds, 1000 = 1 second, measured in ms (convert to s for human eyes lol)
             callback: () => {
                 my.sprite.medium_enemy.direction *= -1;
+                my.sprite.medium_enemy_left.direction *= -1;
             },
             loop: true // Loops the event (repeat)
         });
@@ -142,6 +155,9 @@ class PlanesFight extends Phaser.Scene {
         this.right = this.input.keyboard.addKey("D");
         //this.nextScene = this.input.keyboard.addKey("S");
         this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+        // For small pyramid spawn
+        this.spawnCounter = 0;
 
         // Set movement speeds (in pixels/tick)
         this.playerSpeed = 5;
@@ -202,9 +218,29 @@ class PlanesFight extends Phaser.Scene {
             }
 
             let currentTime = this.time.now;
-            if (currentTime - my.sprite.medium_enemy.lastFired > 1000) {
+            if (currentTime - my.sprite.medium_enemy.lastFired > 3000) {
                 this.fireMediumEnemyBullet(my.sprite.medium_enemy.x, my.sprite.medium_enemy.y);
                 my.sprite.medium_enemy.lastFired = currentTime;
+            }
+        }
+
+        if (my.sprite.medium_enemy_left.visible) {
+            my.sprite.medium_enemy_left.x += my.sprite.medium_enemy_left.direction * my.sprite.medium_enemy_left.speed;
+
+            // Boundary Checking for Medium Enemy
+            if (my.sprite.medium_enemy_left.x <= my.sprite.medium_enemy_left.displayWidth / 2) {
+                my.sprite.medium_enemy_left.x = my.sprite.medium_enemy_left.displayWidth / 2;
+                my.sprite.medium_enemy_left.direction = 1; // Force direction to right
+            }
+            if (my.sprite.medium_enemy_left.x >= game.config.width - my.sprite.medium_enemy_left.displayWidth / 2) {
+                my.sprite.medium_enemy_left.x = game.config.width - my.sprite.medium_enemy_left.displayWidth / 2;
+                my.sprite.medium_enemy_left.direction = -1; // Force direction to left
+            }
+
+            let currentTime = this.time.now;
+            if (currentTime - my.sprite.medium_enemy_left.lastFired > 3000) {
+                this.fireMediumEnemyBullet(my.sprite.medium_enemy_left.x, my.sprite.medium_enemy_left.y);
+                my.sprite.medium_enemy_left.lastFired = currentTime;
             }
         }
 
@@ -285,19 +321,50 @@ class PlanesFight extends Phaser.Scene {
                 }
             }
             if (this.collides(my.sprite.medium_enemy, bullet)){ 
-                // start animation
-                this.puff = this.add.sprite(my.sprite.medium_enemy.x, my.sprite.medium_enemy.y, "whitePuff03").setScale(0.4).play("puff");
+                this.sound.play("smallping", {
+                    volume: 0.8
+                }); // hit sound but lower volume so players know they hit it but plane not down yet 
+                
                 // clear out bullet -- put y offscreen, will get reaped next update
                 bullet.y = -100;
-                my.sprite.medium_enemy.visible = false;
-                my.sprite.medium_enemy.x = -100;
-                // Update score
-                this.myScore += my.sprite.medium_enemy.scorePoints;
-                this.updateScore();
-                // Play sound
+
+                my.sprite.medium_enemy.health -= 1; 
+                if (my.sprite.medium_enemy.health <= 0) {
+                    this.puff = this.add.sprite(my.sprite.medium_enemy.x, my.sprite.medium_enemy.y, "whitePuff03").setScale(0.4).play("puff");
+                    my.sprite.medium_enemy.visible = false;
+                    my.sprite.medium_enemy.x = -100;
+                
+                    // Update score
+                    this.myScore += my.sprite.medium_enemy.scorePoints;
+                    this.updateScore();
+                    // Play sound
+                    this.sound.play("smallping", {
+                        volume: 1   // Can adjust volume using this, goes from 0 to 1
+                    });
+                }
+            }
+
+            if (this.collides(my.sprite.medium_enemy_left, bullet)){ 
                 this.sound.play("smallping", {
-                    volume: 1   // Can adjust volume using this, goes from 0 to 1
+                    volume: 0.8
                 });
+                // clear out bullet -- put y offscreen, will get reaped next update
+                bullet.y = -100;
+
+                my.sprite.medium_enemy_left.health -= 1; 
+                if (my.sprite.medium_enemy_left.health <= 0) {
+                    this.puff = this.add.sprite(my.sprite.medium_enemy_left.x, my.sprite.medium_enemy_left.y, "whitePuff03").setScale(0.4).play("puff");
+                    my.sprite.medium_enemy_left.visible = false;
+                    my.sprite.medium_enemy_left.x = -100;
+                
+                    // Update score
+                    this.myScore += my.sprite.medium_enemy_left.scorePoints;
+                    this.updateScore();
+                    // Play sound
+                    this.sound.play("smallping", {
+                        volume: 1   // Can adjust volume using this, goes from 0 to 1
+                    });
+                }
             }
         }
 
@@ -306,7 +373,8 @@ class PlanesFight extends Phaser.Scene {
             bullet.y -= this.bulletSpeed;
         }
 
-        if (this.areAllEnemiesDefeated()) {
+        if (this.areAllSmallEnemiesDefeated() && this.spawnCounter < 3) {
+            this.spawnCounter += 1; // Increment the spawn counter
             let newX = Phaser.Math.Between(50, 150); // Random X start
             this.spawnEnemyPyramid(newX);
         }
@@ -326,7 +394,34 @@ class PlanesFight extends Phaser.Scene {
     }
 
     gameOver() {
-        this.scene.restart();
+        for (let bullet of this.my.sprite.enemy_bullet) {
+            bullet.destroy();
+        }
+        this.my.sprite.enemy_bullet = [];
+        for (let enemy of this.my.sprite.enemies) {
+            enemy.destroy();
+        }
+        this.my.sprite.enemies = [];
+        for (let health of this.my.sprite.health) {
+            health.destroy();
+        }
+        this.my.sprite.health = [];
+
+        this.myScore = 0;
+
+        this.spawnCounter = 0;
+
+        if (this.my.sprite.medium_enemy) {
+            this.my.sprite.medium_enemy.health = 3;
+        }
+
+        if (this.my.sprite.medium_enemy_left) {
+            this.my.sprite.medium_enemy_left.health = 3;
+        }
+
+        this.time.delayedCall(100, () => {
+            this.scene.restart();
+        }); // apparently you need to give some time for cleanup logic to complete 
     }
 
 }
